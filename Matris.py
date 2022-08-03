f13612d5598d92f37b9ef453d2a4aeea0d25300b
juliocwb <julio.cwb@gmail.com>
2022-08-03 10:21:52 -0300

    upadate

--- Matris.py
+++ Matris.py
@@ -1,1 +1,11 @@
-testando
+mat=[]
+
+for i in range(3):
+    linha=[]
+    for j in range(3):
+        dado = int(input("Digete um valor: "))
+        linha.append(dado)
+    mat.append(linha)
+for i in range(3):
+    for j in range(3):
+        print("Elemento da linha", i , "e Coluna",j, "é:", mat[i][j])
