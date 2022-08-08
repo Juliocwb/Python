87e002fc965c805239e193439bb7b0462625c2ab
juliocwb <julio.cwb@gmail.com>
2022-08-08 09:28:20 -0300

    update 2.1

--- Mapa1.py
+++ Mapa1.py
@@ -1,5 +1,3 @@
-from traceback import print_tb
-
 def existe_pessoa(lista, nome):
    if len(lista) > 0:
      for pessoa in lista:
@@ -30,37 +28,37 @@ def alterar(lista):
                print("estado: ",pessoa['estado'])
                print("status: ",pessoa['status'])
 
-               pessoa['nome'] = input("digite o novo nome do cadastro:")
-               pessoa['telefone'] = input("Digite o novo telefone:")
-               pessoa['cidade'] = input("Digite o novo cidade:")
-               pessoa['estado'] = input("Digite o novo estado:")
-               pessoa['status'] = input("Digite o novo status:")
+               pessoa['nome'] = input("digite o novo nome do cadastro: ")
+               pessoa['telefone'] = input("Digite o novo telefone: ")
+               pessoa['cidade'] = input("Digite o novo cidade: ")
+               pessoa['estado'] = input("Digite o novo estado: ")              
+               pessoa['status'] = input("Digite (P-> Pessoal) ou (C-> Comercial): ")
                break
        else:
-          print(f"Não existe esse nome {nome} no cadastro do sistema")
+          print(f"Não existe esse nome:{nome} no cadastro do sistema \n")
     else:
-       print("nao existe nenhum cadastro no sistema.")
+       print("Não existe nenhum cadastro no sistema. \n")
 
 def listar(lista):
     print("Lista cadastrado")
     if len(lista) > 0:
         for i, pessoa in enumerate(lista):
-            print("Pessoa ",(i+1))
-            print("Nome:  ", pessoa['nome'])
-            print("telefone: ",pessoa['telefone'])
-            print("cidade:  ",pessoa['cidade'])
-            print("estado: ",pessoa['estado'])
-            print("status: ",pessoa['status'])
-        print("Quantidade de pessoa cadastrada: ", (len(lista)))
+            print("Pessoa: ",(i+1))
+            print("Nome: {}".format(pessoa['nome']))
+            print("telefone: {}".format(pessoa['telefone']))
+            print("cidade: {}".format(pessoa['cidade']))
+            print("estado: {}".format(pessoa['estado']))
+            print("status: {}".format(pessoa['status']))
+        print("Quantidade de pessoa cadastrada: ",(len(lista)))
     else:
-      print("Nao existe nenhuma pessoa cadastrada no sistema. ")
+      print("Nao existe nenhuma pessoa cadastrada no sistema. \n")
       
 def procurar(lista):
     print ("Listar cadastro")
     if len(lista) > 0:
-       nome = input("Digita nome da pessoa que deseja procurar  ")
+       nome = input("Digita nome da pessoa que deseja procurar: ")
        if existe_pessoa(lista, nome):
-          print("O cadastro encontrado. As informaçõe segue abaixo: ")
+          print("O cadastro encontrado. As informaçõe segue abaixo: \n")
           for pessoa in lista:
             if pessoa['nome'] == nome:
                print("Nome: ",pessoa['nome'])
@@ -70,15 +68,15 @@ def procurar(lista):
                print("status: ",pessoa['status'])               
                break
        else:
-          print(f"Não existe esse nome {nome} no cadastro do sistema")
+          print(f"Não existe esse nome: {nome} no cadastro do sistema \n")
     else:
-       print("nao existe nenhum cadastro no sistema.")
-  
+       print("nao existe nenhum cadastro no sistema. \n")  
+
 def excluir(lista):
     if len(lista) > 0:
-       nome = input("Digita nome da pessoa que deseja excluir  ")
+       nome = input("Digita nome da pessoa que deseja excluir: \n")
        if existe_pessoa(lista, nome):
-          print("O cadastro encontrado. As informaçõe segue abaixo: ")
+          print("O cadastro encontrado. As informaçõe segue abaixo: \n")
           for i, pessoa in enumerate (lista):
             if pessoa['nome'] == nome:
                print("Nome: ",pessoa['nome'])
@@ -86,14 +84,12 @@ def excluir(lista):
                print("cidade: ",pessoa['cidade'])
                print("estado: ",pessoa['estado'])
                print("status: ",pessoa['status'])
-
                lista.pop(i)
-
                print("Pessoa foi apagada com sucesso!")
        else:
-          print(f"Não existe esse nome {nome} no cadastro do sistema")
+          print(f"Não existe esse nome: {nome} no cadastro do sistema \n")
     else:
-       print("nao existe nenhum cadastro no sistema.")
+       print("nao existe nenhum cadastro no sistema. \n")
 
 def Menu ():
 
@@ -119,8 +115,8 @@ def Menu ():
     elif op == 5:
       excluir(lista)
     elif op == 6:  
-      print("Saindo do sistema")
+      print("Saindo do sistema \n")
       break
     else:
-      print("opção invalida. Escolha outra opção da lista. ")
+      print("Opção invalida. Escolha outra opção da lista. \n")
 Menu()
